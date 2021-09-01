f03d42121af134f6f7291c7fef80afc526752124
Dea Edria <73749289+deaedria@users.noreply.github.com>
2021-09-01 21:14:41 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    env: {
-        API_URI: 'https://newstoday-server.herokuapp.com/news/api/',
-        PUBLIC_URI : 'https://newstoday-server.herokuapp.com'
-    },
-    images: {
-      domains: ['localhost', 'newstoday-server.herokuapp.com']
-    }
-}
