346d1ab78039751846840f8c0285554c8ce0b775
deaedria <dea.edria@gmail.com>
2021-09-08 13:56:09 +0700

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    env: {
-        API_URI: process.env.NEXT_API_URI,
-        PUBLIC_URI : process.env.NEXT_PUBLIC_URI,
-    },
-    images: {
-      domains: ['localhost', 'newstoday-server.herokuapp.com']
-    }
+  generateBuildId: async () => {
+    return 'build'
+  },
+  env: {
+    API_URI: process.env.NEXT_API_URI,
+    PUBLIC_URI: process.env.NEXT_PUBLIC_URI,
+  },
+  images: {
+    domains: ['localhost', 'newstoday-server.herokuapp.com']
+  }
 }
